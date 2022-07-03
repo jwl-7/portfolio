e2a660c50805e95bab3bc9944003046119fce7f3
jwl-7 <lucidrainbows@gmail.com>
2022-07-03 06:00:05 -0400

    Refactor, abstract component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
-import Form from 'react-bootstrap/Form'
+
 import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 import emailjs from 'emailjs-com'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
 import { Fade } from 'react-awesome-reveal'
 
-import { AiFillGithub } from 'react-icons/ai'
-import { AiFillLinkedin } from 'react-icons/ai'
+import { FaGithub } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+
 
 const templateId = process.env.REACT_APP_EMAILJS_TEMPLATEID
 const serviceId = process.env.REACT_APP_EMAILJS_SERVICEID
@@ -20,7 +22,8 @@ const initialFormData = {
     message: ''
 }
 
-const Contact = () => {
+
+function Contact() {
     const [formData, updateFormData] = useState(initialFormData)
     const [validated, setValidated] = useState(false)
 
@@ -68,79 +71,104 @@ const Contact = () => {
     return (
         <Container fluid id='contact'>
             <Row className='justify-content-center'>
-                <Col xs={12} className='text-center'>
-                    <h1>Contact</h1>
-                    <hr />
-                </Col>
-                <Col lg={10} xl={8} xxl={6}>
-                    <Fade direction='up' triggerOnce>
-                        <Card className='m-3'>
-                            <Card.Body>
-                                <Row>
-                                    <Col lg={6} className='p-3'>
-                                        <h3>Send a Message</h3>
-                                        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                                            <Form.Group className='mb-3' controlId='formName'>
-                                                <Form.Label>Name</Form.Label>
-                                                <Form.Control
-                                                    name='name'
-                                                    type='name'
-                                                    required
-                                                    onChange={handleChange}
-                                                />
-                                            </Form.Group>
-                                            <Form.Group className='mb-3' controlId='formEmail'>
-                                                <Form.Label>Email</Form.Label>
-                                                <Form.Control
-                                                    name='email'
-                                                    type='email'
-                                                    required
-                                                    onChange={handleChange}
-                                                />
-                                            </Form.Group>
-                                            <Form.Group className='mb-3' controlId='formMessage'>
-                                                <Form.Label>Message</Form.Label>
-                                                <Form.Control
-                                                    name='message'
-                                                    as='textarea'
-                                                    required
-                                                    onChange={handleChange}
-                                                    rows={3}
-                                                />
-                                            </Form.Group>
-                                            <Button variant='primary' type='submit'>
-                                                Submit
-                                            </Button>
-                                        </Form>
-                                    </Col>
-                                    <Col lg={6} className='p-3'>
-                                        <h3 className='mb-3'>Find Me On</h3>
-                                        <p className='mb-4'>Feel free to connect with me or send me a message.</p>
-                                        <Button
-                                            variant='primary'
-                                            href='https://github.com/jwl-7'
-                                            target='_blank'
-                                            className='me-2 mb-2'
-                                        >
-                                            <h5><AiFillGithub /> GitHub</h5>
-                                        </Button>
-                                        <Button
-                                            variant='primary'
-                                            href='https://linkedin.com/in/jonathanwlusk/'
-                                            target='_blank'
-                                            className='mb-2'
-                                        >
-                                            <h5><AiFillLinkedin /> LinkedIn</h5>
-                                        </Button>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                        </Card>
-                    </Fade>
-                </Col>
+                {renderTitle()}
+                {renderCard(validated, handleChange, handleSubmit)}
             </Row>
         </Container>
     )
 }
+
+function renderTitle() {
+    return (
+        <Col xs={12} className='text-center'>
+            <h1>Contact</h1>
+            <hr />
+        </Col>
+    )
+}
+
+function renderCard(validated, handleChange, handleSubmit) {
+    return (
+        <Col lg={10} xl={8} xxl={6}>
+            <Fade direction='up' triggerOnce>
+                <Card className='m-3'>
+                    <Card.Body>
+                        <Row>
+                            {renderCardForms(validated, handleChange, handleSubmit)}
+                            {renderCardFooter()}
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </Fade>
+        </Col>
+    )
+}
+
+function renderCardForms(validated, handleChange, handleSubmit) {
+    return (
+        <Col lg={6} className='p-3'>
+            <h3>Send a Message</h3>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form.Group className='mb-3' controlId='formName'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        name='name'
+                        type='name'
+                        required
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formEmail'>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        name='email'
+                        type='email'
+                        required
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formMessage'>
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control
+                        name='message'
+                        as='textarea'
+                        required
+                        onChange={handleChange}
+                        rows={3}
+                    />
+                </Form.Group>
+                <Button variant='primary' type='submit'>
+                    Submit
+                </Button>
+            </Form>
+        </Col>
+    )
+}
+
+function renderCardFooter() {
+    return (
+        <Col lg={6} className='p-3'>
+            <h3 className='mb-3'>Find Me On</h3>
+            <p className='mb-4'>Feel free to connect with me or send me a message.</p>
+            <Button
+                variant='primary'
+                href='https://github.com/jwl-7'
+                target='_blank'
+                className='me-2 mb-2'
+            >
+                <h5><FaGithub /> GitHub</h5>
+            </Button>
+            <Button
+                variant='primary'
+                href='https://linkedin.com/in/jonathanwlusk/'
+                target='_blank'
+                className='mb-2'
+            >
+                <h5><FaLinkedin /> LinkedIn</h5>
+            </Button>
+        </Col>
+    )
+}
+
 
 export default Contact
