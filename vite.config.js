b103c863ed021e6f3629fc805d9cc908cfbac2b2
jwl-7 <lucidrainbows@gmail.com>
2024-02-01 17:50:55 -0500

    I just really didn't like that semicolon, the way it looked at me, nah get outta here

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import svgr from 'vite-plugin-svgr';
+import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
