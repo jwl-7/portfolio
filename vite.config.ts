fb5dbc6681cf3edac8935d5f7093d5b23f270fc2
jwl-7 <lucidrainbows@gmail.com>
2024-02-09 00:40:20 -0500

    Convert to TypeScript (no typings added)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
     plugins: [
         react(),
