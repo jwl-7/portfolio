971737e07f4073d67c554426f33955ea6b6d02a5
jwl-7 <lucidrainbows@gmail.com>
2024-02-23 01:49:16 -0500

    Redo app structure to align with standards

---
+++ src/components/App/index.tsx
@@ -0,0 +1,30 @@
+import '@styles/styles.sass'
+
+import React from 'react'
+
+import About from '@components/About'
+import Contact from '@components/Contact'
+import Footer from '@/components/Footer'
+import Home from '@components/Home'
+import NavBar from '@components/NavBar'
+import Projects from '@components/Projects'
+import Resume from '@components/Resume'
+
+
+export default function App() {
+    return (
+        <React.StrictMode>
+            <div className="App">
+                <NavBar />
+                <main>
+                    <Home />
+                    <About />
+                    <Projects />
+                    <Resume />
+                    <Contact />
+                </main>
+                <Footer />
+            </div>
+        </React.StrictMode>
+    )
+}
