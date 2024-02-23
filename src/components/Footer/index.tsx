c377058f0e3b17e68459ed0e20e78898ecfde10c
jwl-7 <lucidrainbows@gmail.com>
2024-02-22 23:36:45 -0500

    Fix overflow

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,10 +3,8 @@ import styles from './footer.module.sass'
 
 export default function Footer() {
     return (
-        <div className={styles.container}>
-            <footer className={styles.footer}>
-                <h4 className={styles.text}>Made by Jonathan Lusk &copy; 2024</h4>
-            </footer>
-        </div>
+        <footer className={styles.footer}>
+            <h4 className={styles.text}>Made by Jonathan Lusk &copy; 2024</h4>
+        </footer>
     )
 }
