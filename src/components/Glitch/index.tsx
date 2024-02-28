ce2cd766d445631b138495825a77585fc6d7d351
jwl-7 <lucidrainbows@gmail.com>
2024-02-28 04:35:30 -0500

    Refactor mobile glitch text

--- src/components/Glitch/index.tsx
+++ src/components/Glitch/index.tsx
@@ -1,10 +1,31 @@
 import styles from './glitch.module.sass'
 
+import { useMediaQuery } from 'react-responsive'
+
 
 export default function Glitch() {
-    return (
-        <div className={styles.glitch} data-text="Jonathan Lusk">
-            Jonathan Lusk
-        </div>
-    )
+    const isScreenMd = useMediaQuery({ maxWidth: 768 })
+
+    const renderDesktop = () => {
+        return (
+            <div className={styles.glitch} data-text="Jonathan Lusk">
+                Jonathan Lusk
+            </div>
+        )
+    }
+
+    const renderMobile = () => {
+        return (
+            <div className={styles.mobileGlitchContainer}>
+                <div className={styles.glitch} data-text="Jonathan">
+                    Jonathan
+                </div>
+                <div className={styles.glitch} data-text="Lusk">
+                    Lusk
+                </div>
+            </div>
+        )
+    }
+
+    return isScreenMd ? renderMobile() : renderDesktop()
 }
