b4298565a72d71c9f961e78745d7055d354b8f1b
jwl-7 <lucidrainbows@gmail.com>
2024-02-28 23:35:16 -0500

    Refactor footer, remove heading tag to align with h tag order accessibility rules

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import styles from './footer.module.sass'
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <h4 className={styles.text}>Made by Jonathan Lusk &copy; 2024</h4>
+            <span className={styles.text}>Made by Jonathan Lusk &copy; 2024</span>
         </footer>
     )
 }
