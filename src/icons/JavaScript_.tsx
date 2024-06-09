3edc2074eec0f01059b45d038fac29f07052c6ba
jwl-7 <lucidrainbows@gmail.com>
2024-06-09 19:05:20 -0400

    tmp

--- src/icons/JavaScript_.tsx
+++ src/icons/JavaScript_.tsx
@@ -1,4 +1,4 @@
-export default function SvgJavascript() {
+export default function SvgJavaScript() {
     return (
         <svg viewBox="0 0 128 128">
             <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185H1.408z" />
